5819a3c0975da6e17b0104a26110eb3e6f7fa968
PrzeChoj <43790001+PrzeChoj@users.noreply.github.com>
2022-05-10 23:44:58 +0200

    Update goal_function_example.R

--- goal_function_example.R
+++ goal_function_example.R
@@ -1,13 +1,16 @@
+install.packages("permutations")
+library(permutations)
+
 install.packages("devtools")
 library(devtools)
 
 devtools::install_github("PrzeChoj/gips")
 library(gips)
 
-c <- permutations::as.cycle(permutations::as.word(c(2,1)))
+c <- as.cycle(as.word(c(2,1)))
 U1 <- matrix(c(1,0.5,0.5,2), nrow=2,byrow = TRUE)
 goal_function(c, 2, 100, U1)
-goal_function(permutations::id, 2, 100, U1)
+goal_function(permutations::id, 2, 100, U1)  # `id` i tak trzeba wołać przez `permutations::id`
 
 
 perm <- as.cycle(as.word(c(1,4,5,2,3,6))) # (1)(3,5)(2,4)(6)
